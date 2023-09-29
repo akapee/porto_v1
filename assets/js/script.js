8d80861664222fc3204492de20b4cb7007e6d8bf
vrazegon <vrazeezarv@gmail.com>
2023-09-29 10:42:12 +0700

    add projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,49 @@ body.addEventListener("click", (e) => {
 });
 
 
+// TYPING TEXT
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Junior Mobile Developer", "Tech Enthusiast"];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
 //FILTERTABS SKILLS
 const skillsBtn = document.querySelectorAll('.skillsTab-btn');
 const skillItem = document.querySelectorAll('.cards_item_skills');
@@ -78,6 +121,9 @@ function showSkillsItem(newMenuBtn){
     });
 }
 
+
+
+// FILTERTABS PROJECTS
 const projectsBtn = document.querySelectorAll('.projectsTab-btn');
 const projectsItem = document.querySelectorAll('.cards_item_projects');
 
@@ -109,9 +155,6 @@ function showProjectsItem(newMenuBtn){
         }
     });
 }
-
-
-// FILTERTABS PROJECTS
 
 
 
